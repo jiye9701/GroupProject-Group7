3550d0c5de4ba00ed0f421a19b50f03c6fa2c3f5
John Doe <jkim458@my.centennialcollge.ca>
2020-11-05 16:58:16 -0500

    build data in data.js

---
+++ client/data.js
@@ -0,0 +1,32 @@
+module.exports = function () {
+  return {
+    books: [
+      {
+        id: 1,
+        name: "Blood Music",
+        author: "Greg Bear",
+        publised: "1985",
+        description: "jiHyeok Kim    ",
+        price: 43.95,
+        published: "1985",
+      },
+      {
+        id: 2,
+        name: "I Robot",
+        author: "Isaac Asimov",
+        published: "1950",
+        description: "dkfdkfndfndkfndlkfdlk",
+        price: 34.22,
+      },
+      {
+        id: 3,
+        name: "Momo",
+        author: "dontknow",
+        published: "dontknow",
+        description: "dksnsdlkndlkfndlfkddlkf\r\n ",
+        price: 99.33,
+      }
+    ],
+    orders: []
+  };
+};
