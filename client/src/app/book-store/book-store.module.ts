3c2ff5616ad535f17cf1653763bf57b277d3b424
John Doe <jkim458@my.centennialcollge.ca>
2020-11-06 17:28:00 -0500

    Generate CartSummary component

--- client/src/app/book-store/book-store.module.ts
+++ client/src/app/book-store/book-store.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { ModelModule } from '../model/model.module';
 import { BookStoreComponent } from '../book-store/book-store.component';
 import { CounterDirective } from './counter.directive';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [BookStoreComponent, CounterDirective],
+    declarations: [BookStoreComponent, CounterDirective, CartSummaryComponent],
     exports: [BookStoreComponent, CounterDirective]
 })
 export class BookStoreModule {}
