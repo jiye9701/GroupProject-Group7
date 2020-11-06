ad52ced95fe014262b502e2587d519a1b31863a2
John Doe <jkim458@my.centennialcollge.ca>
2020-11-06 16:38:26 -0500

    Create new features modules but doen't work..

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,34 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookStoreModule } from './book-store/book-store.module';
 
-import { HeaderComponent } from './partials/header/header.component';
-import { FooterComponent } from './partials/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { BasePageComponent } from './partials/base-page/base-page.component';
+import { HeaderComponent } from './partials/header/header.component'; // Remove it later after find out what problem is to connect partials and pages module
+import { FooterComponent } from './partials/footer/footer.component'; // Remove it later after find out what problem is to connect partials and pages module
+import { HomeComponent } from './pages/home/home.component'; // Remove it later after find out what problem is to connect partials and pages module
+import { AboutComponent } from './pages/about/about.component'; // Remove it later after find out what problem is to connect partials and pages module
+import { ContactComponent } from './pages/contact/contact.component'; // Remove it later after find out what problem is to connect partials and pages module
+import { ProductsComponent } from './pages/products/products.component'; // Remove it later after find out what problem is to connect partials and pages module
+import { ServicesComponent } from './pages/services/services.component'; // Remove it later after find out what problem is to connect partials and pages module
+import { BasePageComponent } from './partials/base-page/base-page.component'; // Remove it later after find out what problem is to connect partials and pages module
 
-// import { BookStoreComponent } from './book-store/book-store.component'; - Remove this one!! Because we already import and export this in book-store.module.ts
+//import { PartialsModule } from './partials/partials.module'; // Remove comment later after find out what problem is to connect partials and pages module
+//import { PagesModule } from './pages/pages.module'; // Remove comment later after find out what problem is to connect partials and pages module
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    ProductsComponent,
-    ServicesComponent,
-    BasePageComponent
-    // BookStoreComponent - Remove this one!! Because we already import and export this in book-store.module.ts
+    HeaderComponent, // Remove it later after find out what problem is to connect partials and pages module
+    FooterComponent, // Remove it later after find out what problem is to connect partials and pages module
+    HomeComponent, // Remove it later after find out what problem is to connect partials and pages module
+    AboutComponent, // Remove it later after find out what problem is to connect partials and pages module
+    ContactComponent, // Remove it later after find out what problem is to connect partials and pages module
+    ProductsComponent, // Remove it later after find out what problem is to connect partials and pages module
+    ServicesComponent, // Remove it later after find out what problem is to connect partials and pages module
+    BasePageComponent // Remove it later after find out what problem is to connect partials and pages module
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BookStoreModule
+    BookStoreModule,
+    //PartialsModule, // Remove comment later after find out what problem is to connect partials and pages module
+    //PagesModule // Remove comment later after find out what problem is to connect partials and pages module
   ],
   providers: [],
   bootstrap: [AppComponent]
